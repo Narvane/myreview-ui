a1cafa6abed1d5ff9764eca2312e1ff0d57ef5d5
Gustavo Alves de Andrade <gustavoalvesandrade18@gmail.com>
2020-01-07 19:21:06 -0300

    Deploy 13

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/myreview-ui'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/establishments.html'));
+res.sendFile(path.join(__dirname+'/dist/myreview-ui/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-console.log('Console listening')
