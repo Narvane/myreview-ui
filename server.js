e1693050e5ce6c1f550f8db1b46b7f551691dfa0
Gustavo Alves de Andrade <gustavoalvesandrade18@gmail.com>
2020-01-07 18:00:14 -0300

    Deploy 5

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/<myreview-ui>'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<myreview-ui>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/<myreview-ui>/'));
 });
 
 // Start the app by listening on the default Heroku port
