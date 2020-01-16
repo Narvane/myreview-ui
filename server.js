71c4c57c03d15681b9dc92901a6f0f30549b3a59
Gustavo Alves de Andrade <gustavoalvesandrade18@gmail.com>
2020-01-16 15:41:30 -0300

    CORS TEST 3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
+app.use(cors());
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/myreview-ui'));
 
@@ -14,4 +14,3 @@ res.sendFile(path.join(__dirname+'/dist/myreview-ui/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-app.options('*', cors());
