5b393bc578b00fcac8f3497f214c2182d43be879
Gustavo Alves de Andrade <gustavoalvesandrade18@gmail.com>
2020-01-16 15:30:26 -0300

    Teste CORS

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
+app.options('*', cors())
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/myreview-ui'));
-app.options('*', cors())
+
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/myreview-ui/index.html'));
